8960c2925019f625a79387251389237962d36192
Sherlock Ma <xizma@ucdavis.edu>
2020-02-10 10:42:52 -0800

    complete SymbolTable&SymbolEntry

--- nada/SymbolEntry.java
+++ nada/SymbolEntry.java
@@ -2,13 +2,16 @@ package nada;
 
 public class SymbolEntry extends Object{
 
-    //public static final int NONE = 0;
+    // NONE is used when only doing scope analysis
+    // When role analysis is activated, NONE shouldn't be used anywhere
+    public static final int NONE = 0;
     public static final int CONST = 1;
     public static final int PARAM = 2;
     public static final int PROC = 3;
     public static final int TYPE = 4;
     public static final int VAR = 5;
- 
+
+
     // name is just the identifier name
     private String name;
     // role should be one of the five listed above
@@ -21,25 +24,38 @@ public class SymbolEntry extends Object{
         this.role = role;
     }
  
-    public String toString(int mode){
-        if (mode == Parser.ROLE){
+    public String toString(boolean roleEnabled){
+        if (roleEnabled == true){
            return "Name: " + this.name + " " + "Role: " + roleToString();
         }
         else{
            return "Name: " + this.name;
         }
     }
- 
+
+    @Override
+    public String toString(){
+        this.toString(false);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getRole(){
+        return this.role;
+    }
+
     private String roleToString(){
         String s = "";
         switch (role){
-            //case NONE:  s = "None";      break;
+            case NONE:  s = "None";      break;
             case CONST: s = "CONSTANT";  break;
             case PARAM: s = "PARAMETER"; break;
             case PROC:  s = "PROCEDURE"; break;
             case TYPE:  s = "TYPE";      break;
             case VAR:   s = "VARIABLE";  break;
-            //default:    s = "None";
+            default:    s = "None";
         }
         return s;
     }
---
+++ nada/SymbolTable.java
@@ -0,0 +1,95 @@
+package nada;
+import java.util.*;
+
+public class SymbolTable {
+    private Stack<Map<String, SymbolEntry>> stack;
+    // since Stack is already equipped with method size(),
+    // no need to define a separate variable level
+    // simple_mode is used to just report name
+    // complete_mode is used to report both name and role
+    boolean roleEnabled;
+
+    // constructor, pushes the first layer of map and loads predefined identifiers
+    public SymbolTable(boolean roleEnabled){
+        this.roleEnabled = roleEnabled;
+        this.stack = new Stack<Map<String, SymbolEntry>>();
+        HashMap<String, SymbolEntry> map = new HashMap<String, SymbolEntry>();
+        if(roleEnabled == true){
+            map.put("BOOLEAN", new SymbolEntry("BOOLEAN", SymbolEntry.TYPE));
+            map.put("CHAR", new SymbolEntry("CHAR", SymbolEntry.TYPE));
+            map.put("INTEGER", new SymbolEntry("INTEGER", SymbolEntry.TYPE));
+            map.put("TRUE", new SymbolEntry("TRUE", SymbolEntry.CONST));
+            map.put("FALSE", new SymbolEntry("FALSE", SymbolEntry.CONST));
+        }
+        else{
+            map.put("BOOLEAN", new SymbolEntry("BOOLEAN", SymbolEntry.NONE));
+            map.put("CHAR", new SymbolEntry("CHAR", SymbolEntry.NONE));
+            map.put("INTEGER", new SymbolEntry("INTEGER", SymbolEntry.NONE));
+            map.put("TRUE", new SymbolEntry("TRUE", SymbolEntry.NONE));
+            map.put("FALSE", new SymbolEntry("FALSE", SymbolEntry.NONE));
+        }
+        this.stack.push(map);
+    }
+
+
+    // enter a new scope
+    public void enterScope(){
+        this.stack.push(new HashMap<String, SymbolEntry>());
+    }
+
+    // exit an existing scope
+    // if exit successfully, return true
+    // else, there is no map to exit, return false
+    // printInfo controls whether it should print the info cantained in the map being popped
+    public boolean exitScope(boolean printInfo){
+        if(this.stack.empty()){
+            // empty table, return false
+            return false;
+        }
+        Map<String, SymbolEntry> map = this.stack.pop();
+        if(printInfo){
+            System.out.println("\nLevel " + this.stack.size() + ":");
+            System.out.println("---------");
+            for(SymbolEntry s : map.values()){
+                System.out.println(s.toString(this.roleEnabled));
+            }
+        }
+        return true;
+    }
+
+    // returns:
+    // true iff successfully entered a SymbolEntry
+    // false iff this identifier already exists in the topmost map
+    public boolean enterSymbol(final SymbolEntry s){
+        Map<String, SymbolEntry> map = this.stack.peek();
+        if(map.containsKey(s.getName())){
+            return false;
+        }
+
+        // insert it into the map
+        map.put(s.getName(), s);
+        return true;
+    }
+
+    // returns:
+    // the SymbolEntry it can first get by searching the table one layer after another
+    // if the SymbolEntry cannot be found, null is returned
+    public SymbolEntry findSymbol(String name){
+        for(int i = this.stack.size() - 1; i >= 0; i--){
+            Map<String, SymbolEntry> map = this.stack.get(i);
+            SymbolEntry s = map.get(name);
+            if(s != null){
+                return s;
+            }
+        }
+
+        // it does not find the symbol
+        // in this case, null is returned
+        return null;
+    }
+
+    // return the level of the topmost map
+    public int getLevel(){
+        return stack.size() - 1;
+    }
+}
